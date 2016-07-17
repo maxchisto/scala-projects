a7ef04dbfedccd93b0f9205ed9cd2cfd335b3057
Max C <max.chisto@gmail.com>
2016-07-17 13:31:12 -0700

    iosApp initial commit

---
+++ iosApps/src/main/scala/hi.scala
@@ -0,0 +1,10 @@
+object Hi { 
+    def main(args: Array[String]) = println("Hi!")
+}
+
+class iOSApp (id: Int) {
+    val name: String
+    val sumamry: String
+    val price: Int
+    val numberOfReviews: Int
+}
